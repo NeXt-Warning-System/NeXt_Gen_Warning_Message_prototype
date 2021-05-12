4067c797008007a4b1fdf74b4f9f7bb7fd21673f
christina gyles <christina.gyles@environment-agency.gov.uk>
2021-05-12 10:00:08 +0100

    updates to the prototype to detail the intent behind each version, or test as I've renamed

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'NeXt Generation Warning System',
+  serviceName: 'Get flood warnings',
 
   // Default port that prototype runs on
   port: '3000',
